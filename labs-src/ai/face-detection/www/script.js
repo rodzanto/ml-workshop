cd5f632167f88fb0307ee6f201fec32e28b0a105
Carlos Afonso Perez <cafonsop@amazon.com>
2020-05-18 10:21:33 +0200

    Improve face detection lab, implementation complete

--- labs-src/ai/face-detection/www/script.js
+++ labs-src/ai/face-detection/www/script.js
@@ -33,7 +33,7 @@ let rekognitionClient = new AWS.Rekognition();
 var emotionTexts = {
     "CALM": "Tengo sueño, ¡necesito la energía de AWS!",
     "SAD": "Los servidores on-prem me entristecen...",
-    "CONFUSED": "No entiendo esta demo, voy a preguntarle a un SA",
+    "CONFUSED": "No entiendo esta demo, voy a preguntarle a un SA.",
     "HAPPY": "¡Me encanta la inteligencia artificial!",
     "ANGRY": "El hambre me tiene de mal humor...",
     "DISGUSTED": "¿Algún médico en la sala?",
@@ -50,83 +50,100 @@ function dataURItoBlob(dataURI) {
 }
 
 function renderLabels(labels) {
-    $("#labelResults").empty();
-    let labelTemplate = "<div><span class='rekLabel'></span> - <span class='rekScore'></span> %</div>";
-    $.each(labels, function(index, value) {
-        if (value.Confidence > 50) {
-            let LabelToAdd = $(labelTemplate);
-            $(LabelToAdd).find(".rekLabel").text(value.Name.toLowerCase());
-            $(LabelToAdd).find(".rekScore").text(Math.floor(value.Confidence));
-            $("#labelResults").append(LabelToAdd);
+    let labelTemplate = `<div class="label">
+          <span class="labelName"></span>
+          <div class="confidencePctBar">
+            <div class="confidencePctBarValue"></div>
+          </div>
+        </div>`;
+    let labelContainer = $('#labels');
+    labelContainer.empty();
+
+    labels.slice(0, 10).forEach(label => {
+        if (label.Confidence > 50) {
+            let labelElement = $(labelTemplate);
+            labelElement.find('.labelName').text(label.Name.toUpperCase());
+            labelElement.find('.confidencePctBarValue').css('width', Math.floor(label.Confidence) + '%');
+            labelContainer.append(labelElement);
         }
     });
 }
 
-function renderFaces(faces) {
+function renderFaces(faces, camWidth, camHeight) {
+    let faceboxTemplate = `<div class="facebox">
+        <div class="emotion">
+          <span class="emotionName"></span>
+          <div class="confidencePctBar">
+            <div class="confidencePctBarValue"></div>
+          </div>
+        </div>
+      </div>`;
+
+    let faceboxes = $('#faceboxes');
+    faceboxes.empty();
+
     $("#faceResults").empty();
     $("#faceBox").empty();
+
+    let frameOffset = $('#videoFrame').offset();
+    let frameOffsetLeft = frameOffset.left;
+    let frameOffsetTop = frameOffset.top;
+
     if (faces.length > 0) {
-        labelTemplate = "<div><span class='rekType'></span> - <span class='rekScore'></span> %</div>";
-        $.each(faces[0].Emotions, function(index, value) {
-            if (value.Confidence > 30) {
-                let LabelToAdd = $(labelTemplate); $(LabelToAdd).find(".rekType").text(value.Type.toLowerCase()); $(LabelToAdd).find(".rekScore").text(Math.floor(value.Confidence)); $("#faceResults").append(LabelToAdd);
-                if (mainEmotion == null || (mainEmotion != null && value.Confidence > mainEmotion.Confidence)) {
-                    mainEmotion = value;
+        faces.forEach(face => {
+            let mainEmotion = null;
+
+            for (var i in face.Emotions) {
+                let emotion = face.Emotions[i];
+                if (mainEmotion === null || (mainEmotion !== null && emotion.Confidence > mainEmotion.Confidence)) {
+                    mainEmotion = emotion;
                 }
             }
+
+            let facebox = $(faceboxTemplate);
+            facebox.css('top', (frameOffsetTop + face.BoundingBox.Top * camHeight) + "px");
+            facebox.css('left', (frameOffsetLeft + face.BoundingBox.Left * camWidth) + "px");
+            // Extend height to account for label and confidence bar
+            facebox.css('height', (face.BoundingBox.Height * camHeight) + 30 + "px");
+            facebox.css('width', (face.BoundingBox.Width * camWidth) + "px");
+
+            if (mainEmotion != null && mainEmotion.Confidence > 30 && mainEmotion.Type in emotionTexts) {
+                facebox.find('.emotionName').text(mainEmotion.Type);
+                facebox.find('.confidencePctBarValue').css('width', Math.floor(mainEmotion.Confidence) + '%');
+            }
+
+            faceboxes.append(facebox);
         });
-
-        // bounding box
-        let box = document.getElementById("box")
-        box.style.left = (faces[0].BoundingBox.Left * camWidth) + "px";
-        box.style.width = (faces[0].BoundingBox.Width * camWidth) + "px";
-        box.style.top = (faces[0].BoundingBox.Top * camHeight) + "px";
-        box.style.height = (faces[0].BoundingBox.Height * camHeight) + "px";
-
-        // speech bubbles
-        let speechBubble = document.getElementById("speechBubble");
-        speechBubble.style.left = ((faces[0].BoundingBox.Left * camWidth) + (faces[0].BoundingBox.Width * camWidth)) + "px";
-        speechBubble.style.top = ((faces[0].BoundingBox.Top * camHeight) - 100) + "px";
-        let faceResults = document.getElementById("faceResults");
-        faceResults.style.top = (faces[0].BoundingBox.Top * camHeight - 50) + "px";
-        faceResults.style.left = (faces[0].BoundingBox.Left * camWidth - 20) + "px";
-        let faceBox = document.getElementById("faceBox");
-        faceBox.style.top = (faces[0].BoundingBox.Top * camHeight) + "px";
-        faceBox.style.left = (faces[0].BoundingBox.Left * camWidth) + "px";
-        faceBox.style.height = (faces[0].BoundingBox.Height * camHeight) + "px";
-        faceBox.style.width = (faces[0].BoundingBox.Width * camWidth) + "px";
-        if (mainEmotion != null && mainEmotion.Confidence > 30 && mainEmotion.Type in emotionTexts) {
-            speechBubble.innerText = emotionTexts[mainEmotion.Type];
-            speechBubble.style.visibility = "visible";
-            faceResults.style.visibility = "visible";
-            faceBox.style.visibility = "visible";
-        }
-    } else {
-        // no faces onscreen
-        document.getElementById("speechBubble").style.visibility = "hidden";
-        document.getElementById("faceResults").style.visibility = "hidden";
-        document.getElementById("faceBox").style.visibility = "hidden";
     }
 }
 
-function renderCelebrities(celebrities) {
-    $("#faceCelebrity").empty();
+function renderCelebrities(celebrities, camWidth, camHeight) {
+    let celebrityTemplate = `<div>
+            <span>Celebrity similarity:</span>
+            <span class="celebrityName"></span>
+            <div class="confidencePctBar">
+                <div class="confidencePctBarValue"></div>
+            </div>
+        </div>`;
+
+    let celebrityMatch = $('#celebrityMatch');
+    celebrityMatch.empty();
+
+    let celebrityElement = $(celebrityTemplate);
+    console.log(celebrityElement.find('.celebrityName'));
+
     if (celebrities.length > 0) {
-        let faceCelebrity = document.getElementById("faceCelebrity");
-        faceCelebrity.style.top = (celebrities[0].Face.BoundingBox.Top * camHeight) + "px";
-        faceCelebrity.style.left = (celebrities[0].Face.BoundingBox.Left * camWidth) + "px";
-        faceCelebrity.style.height = (celebrities[0].Face.BoundingBox.Height * camHeight) + "px";
-        faceCelebrity.style.width = (celebrities[0].Face.BoundingBox.Width * camWidth) + "px";
-        faceCelebrity.innerHTML = celebrities[0].Name + ' - ' + data.CelebrityFaces[0].MatchConfidence + '%';
-        faceCelebrity.style.visibility = "visible";
+        celebrityElement.find('.celebrityName').text(celebrities[0].Name.toUpperCase());
+        celebrityElement.find('.confidencePctBarValue').css('width', Math.floor(celebrities[0].MatchConfidence) + '%');
     } else {
-        document.getElementById("faceCelebrity").style.visibility = "hidden";
+        celebrityElement.find('.celebrityName').text('-- NO MATCH --');
+        celebrityElement.find('.confidencePctBarValue').css('width', '0');
     }
+
+    celebrityMatch.append(celebrityElement);
 }
 
 function processFrame(camWidth, camHeight) {
-    let mainEmotion = null
-
     Webcam.snap(function(data_uri) {
         let imageBlob = dataURItoBlob(data_uri);
 
@@ -148,8 +165,8 @@ function processFrame(camWidth, camHeight) {
             //@endExercise
         });
 
-        //@beginExercise
         let facesPromise = new Promise((resolve, reject) => {
+            //@beginExercise
             let detectFacesParams = {
                 Attributes: ["ALL"],
                 Image: {
@@ -160,15 +177,15 @@ function processFrame(camWidth, camHeight) {
                 if (err) {
                     reject(err);
                 } else {
-                    renderFaces(data.FaceDetails);
+                    renderFaces(data.FaceDetails, camWidth, camHeight);
                     resolve(data);
                 }
             });
+            //@endExercise
         });
-         //@endExercise
 
-        //@beginExercise
         let celebritiesPromise = new Promise((resolve, reject) => {
+            //@beginExercise
             let recognizeCelebritiesParams = {
                 Image: {
                     Bytes: imageBlob
@@ -178,12 +195,12 @@ function processFrame(camWidth, camHeight) {
                 if (err) {
                     reject(err);
                 } else {
-                    renderCelebrities(data.CelebrityFaces);
+                    renderCelebrities(data.CelebrityFaces, camWidth, camHeight);
                     resolve(data);
                 }
             });
+            //@endExercise
         });
-        //@endExercise
 
         // Promise.all([labelsPromise, facesPromise, celebritiesPromise])
         //     .catch(() => {
@@ -217,6 +234,6 @@ window.onload = () => {
         let camWidth = cam.videoWidth;
         let camHeight = cam.videoHeight;
 
-        setInterval(processFrame.bind(null, camWidth, camHeight), 4000);
+        setInterval(processFrame.bind(null, camWidth, camHeight), 3000);
     });
 }
